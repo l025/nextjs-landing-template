21551703df0652ad57d7e99daabedd4b531f01fd
mehdi <=>
2024-07-06 07:19:09 +0330

    init

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Sample Landing</title>
+      </Head>
+      <main className="flex min-h-screen"></main>
+    </>
+  );
+}
